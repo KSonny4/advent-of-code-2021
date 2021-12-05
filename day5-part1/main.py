747576e5c262c3891ddc828e5c369476046a955d
Petr Kubelka <petr.kubelka@avast.com>
2021-12-05 14:24:49 +0100

    day5 part2

---
+++ day5-part1/main.py
@@ -0,0 +1,87 @@
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import Generator
+
+
+@dataclass
+class Coordinates:
+    x: int
+    y: int
+
+
+@dataclass
+class Pipe:
+    start: Coordinates
+    end: Coordinates
+
+    @property
+    def x(self) -> int:
+        # use this to easily acccess x when they are same
+        return self.start.x
+
+    @property
+    def y(self) -> int:
+        # use this to easily acccess y when they are same
+        return self.start.y
+
+    def y_range(self) -> Generator[int, None, None]:
+        if self.start.y > self.end.y:
+            return range(self.end.y, self.start.y + 1)
+        if self.start.y < self.end.y:
+            return range(self.start.y, self.end.y + 1)
+
+    def x_range(self) -> Generator[int, None, None]:
+        if self.start.x > self.end.x:
+            return range(self.end.x, self.start.x + 1)
+        if self.start.x < self.end.x:
+            return range(self.start.x, self.end.x + 1)
+
+    @property
+    def is_same_x_coord(self) -> bool:
+        return self.start.x == self.end.x
+
+    @property
+    def is_same_y_coord(self) -> bool:
+        return self.start.y == self.end.y
+
+    @property
+    def is_same_x_or_y_coord(self) -> bool:
+        return self.is_same_x_coord or self.is_same_y_coord
+
+
+def main():
+    with open("input.txt", encoding="utf-8") as f:
+        raw_input = [x.strip().split("->") for x in f.readlines()]
+    data = [
+        [list(map(int, y.split(","))) for y in list(map(str.strip, x))]
+        for x in raw_input
+    ]
+
+    pipes = [Pipe(start=Coordinates(*x[0]), end=Coordinates(*x[1])) for x in data]
+
+    filtered_pipes = [x for x in pipes if x.is_same_x_or_y_coord]
+
+    crosses = defaultdict(dict)
+
+    # Layout where pipe lays in grid
+    for pipe in filtered_pipes:
+        if pipe.is_same_x_coord:
+            for y_pos in pipe.y_range():
+                crosses[pipe.x][y_pos] = crosses.get(pipe.x, {}).get(y_pos, 0) + 1
+        if pipe.is_same_y_coord:
+            for x_pos in pipe.x_range():
+                crosses[x_pos][pipe.y] = crosses.get(x_pos, {}).get(pipe.y, 0) + 1
+
+    # filter out pipe hits > 2
+    pipe_hits = [x.values() for x in crosses.values()]
+    crossings = 0
+    for pipe in pipe_hits:
+        for hit in pipe:
+            if hit > 1:
+                crossings += 1
+    print(crossings)
+
+
+if __name__ == "__main__":
+    main()
+
