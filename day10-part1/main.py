0a4367843c48fe46b9327176fb1b4033bb68a342
Petr Kubelka <petr.kubelka@avast.com>
2021-12-10 12:47:20 +0100

    day 10

---
+++ day10-part1/main.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+
+def check(string: str) -> Optional[str]:
+    q = []
+    vals = {
+        "{": "}",
+        "[": "]",
+        "(": ")",
+        "<": ">",
+    }
+
+    for s in string:
+        if s in vals.keys():
+            s_opposite = vals[s]
+            q.insert(0, s_opposite)
+        else:
+            if s != q[0]:
+                print(f"Expected {q[0]}, got {s}")
+                return s
+            q.pop(0)
+    return None
+
+
+def main():
+    price = {
+        "{": 1197,
+        "[": 57,
+        "(": 3,
+        "<": 25137,
+        "}": 1197,
+        "]": 57,
+        ")": 3,
+        ">": 25137,
+    }
+
+    with open("input.txt", encoding="utf-8") as f:
+        data = [x.strip() for x in f.readlines()]
+
+    total = 0
+    for string in data:
+      result = check(string)
+      if result:
+        total += price[result]
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
---
+++ day10-part2/main.py
@@ -0,0 +1,60 @@
+from typing import Union,List
+
+price = {
+    "{": 3,
+    "[": 2,
+    "(": 1,
+    "<": 4,
+    "}": 3,
+    "]": 2,
+    ")": 1,
+    ">": 4,
+}
+
+def get_total_score(result:str) -> int:
+  total = 0
+  for x in result:
+    total *= 5
+    total += price[x]
+  return total
+
+def check(string: str) -> Union[str, List[str]]:
+    q = []
+    vals = {
+        "{": "}",
+        "[": "]",
+        "(": ")",
+        "<": ">",
+    }
+
+    for s in string:
+        if s in vals.keys():
+            s_opposite = vals[s]
+            q.insert(0, s_opposite)
+        else:
+            if s != q[0]:
+                print(f"Expected {q[0]}, got {s}")
+                return s
+            q.pop(0)
+    return q
+
+
+def main():
+
+
+    with open("input.txt", encoding="utf-8") as f:
+        data = [x.strip() for x in f.readlines()]
+
+    results = []
+    for string in data:
+      result = check(string)
+      if isinstance(result, list):
+        total = get_total_score(result)
+        results.append(total)
+    results = sorted(results)
+    print(results[len(results)//2])
+
+
+
+if __name__ == "__main__":
+    main()
