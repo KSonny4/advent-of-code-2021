b4b18258e1bf944502c1435bf8c02009498211a7
Petr Kubelka <petr.kubelka@avast.com>
2021-12-14 18:14:38 +0100

    reformat

--- day13-part1/main.py
+++ day13-part1/main.py
@@ -2,17 +2,19 @@ import numpy as np
 
 PART1 = True
 
-def fold_by_x(arr: np.array, loc: int)-> np.array:
-    first = np.flipud(arr[(loc + 1):, :])
-    arr[(2 * loc - len(arr) + 1):loc, :] += first
+
+def fold_by_x(arr: np.array, loc: int) -> np.array:
+    first = np.flipud(arr[(loc + 1) :, :])
+    arr[(2 * loc - len(arr) + 1) : loc, :] += first
     arr = arr[:loc, :]
     return arr
 
 
 def fold_by_y(arr: np.array, loc: int) -> np.array:
-    first = np.fliplr(arr[:, (loc + 1):])
-    arr[:, (2 * loc - len(arr[0]) + 1):loc] += first
+    first = np.fliplr(arr[:, (loc + 1) :])
+    arr[:, (2 * loc - len(arr[0]) + 1) : loc] += first
     return arr[:, :loc]
+
 
 def main():
     coords = []
@@ -23,11 +25,10 @@ def main():
         line_split = list(map(int, line.strip().split(",")))
         coords.append(tuple(line_split))
 
-
     max_x = max([x[0] for x in coords])
     max_y = max([x[1] for x in coords])
 
-    arr = np.zeros((max_x + 1,max_y + 1))
+    arr = np.zeros((max_x + 1, max_y + 1))
 
     for coordinate in coords:
         arr[coordinate] = 1
@@ -48,9 +49,8 @@ def main():
             print(np.count_nonzero(arr))
             return
 
-
-    arr[arr > 0] = 77 # Convert all vals > 0 to some more visible num
-    arr = np.transpose(arr) # Transpose so we can print it
+    arr[arr > 0] = 77  # Convert all vals > 0 to some more visible num
+    arr = np.transpose(arr)  # Transpose so we can print it
     print(arr)
     letters = np.hsplit(arr, 8)
     for letter in letters:
@@ -58,6 +58,6 @@ def main():
         print()
     return
 
+
 if __name__ == "__main__":
     main()
-
--- day14-part2/main.py
+++ day14-part2/main.py
@@ -1,5 +1,6 @@
 from collections import Counter
 
+
 def main():
     with open("input.txt", encoding="utf-8") as f:
         string, mappings = f.read().split("\n\n")
@@ -10,16 +11,18 @@ def main():
     for letter in string:
         counts[letter] = counts.get(letter, 0) + 1
 
-    string_set = Counter(string[n:n+2] for n in range(len(string)) if len(string[n:n+2]) == 2 )
+    string_set = Counter(
+        string[n : n + 2] for n in range(len(string)) if len(string[n : n + 2]) == 2
+    )
 
     iteration = 40
     for _ in range(1, iteration + 1):
 
         new_set = {}
         to_delete = {}
-        for (left,right), count in string_set.items():
+        for (left, right), count in string_set.items():
 
-            letter = mappings.get(left+right)
+            letter = mappings.get(left + right)
             if not letter:
                 continue
 
@@ -28,7 +31,7 @@ def main():
             new_set[letter + right] = new_set.get(letter + right, 0) + count
 
             # Remove counter pairs (-1 to the pair)
-            to_delete[left+right] = to_delete.get(left+right, 0) - count
+            to_delete[left + right] = to_delete.get(left + right, 0) - count
 
             counts[letter] = counts.get(letter, 0) + count
 
@@ -40,7 +43,8 @@ def main():
 
     vals = counts.values()
     print(counts)
-    print(max(vals)-min(vals))
+    print(max(vals) - min(vals))
+
 
 if __name__ == "__main__":
     main()
